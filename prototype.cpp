18a1c1faf3701f208f6f6296f79145e6bb119d5b
CodeMriDub7 <mritunjaydubey00@gmail.com>
2026-05-21 19:25:32 +0530

    Making to linked list to store the object address

--- prototype.cpp
+++ prototype.cpp
@@ -3,11 +3,14 @@
 #include <vector>
 #include <bits/stdc++.h>
 using namespace std;
-string designation[4] = {
+
+string designation[6] = {
     "Examination Controller",
     "Deputy Examination Controller",
     "Senior Faculty",
-    "Junior Faculty"
+    "Junior Faculty",
+    "HOD",
+    "Assistant Professor"
 };
 struct name{
     string fname;
@@ -18,71 +21,102 @@ struct DOB{
     int month;
     int year;
 };
-
 /* Faculty class definition */
 class faculty{
     private:
         string password;
-        int id;
-        int des;
-    public:
-        name name;
+        int desig;
         DOB dob;
         long int phone;
+    public:
+        string department;
+        int id;
+        name name;
         string email;
 
         faculty(string fname, string lname){
             this->name.fname = fname;
             this->name.lname = lname;
 
-            cout << "~~~~~~~~~~~~~~~~~~~~~~~~~~~~"<< endl << "Faculty Onboarding Initiated" << endl << "Phone Number: ";
+            cout << "~~~~~~~~~~~~~~~~~~~~~~~~~~~~"<< endl << "Faculty Onboarding Initiated"  
+            << " for "<< name.fname << " " << name.lname << endl << "Phone Number: ";
             cin >> phone;
             cout << "Email: ";
             cin >> email;
-            cout << "What is your designation ?" << endl << "1. Examination Controller "<< endl << "2. Deputy Examination Controller " << endl << "3. Senior Faculty " << endl << "4. Junior Faculty" << endl << "Enter the corresponding number: ";
-            cin >> des;
+            cout << "Department: ";
+            cin >> department;
+            cout << "What is your designation ?" << endl 
+            << "1. Examination Controller "<< endl 
+            << "2. Deputy Examination Controller " << endl 
+            << "3. Senior Faculty " << endl 
+            << "4. Junior Faculty" << endl 
+            << "5. HOD" << endl 
+            << "6. Assistant Professor" << endl 
+            << "Enter the corresponding number: ";
+            cin >> desig;
             cout << "Date of Birth (DD MM YYYY): ";
             cin >> dob.day >> dob.month >> dob.year;
             cout << "Set a password: ";
             cin >> password;
-            id = phone%10000 + dob.day;
+            id = phone%1000;
+            insert (id, this);
             cout << "~~~~~~~~~~~~~~~~~~~~~~~~~~~~" << endl;
-            cout << "Faculty Onboarding Successful!" << endl << "User ID: " << id << endl << "Name: " << name.fname << " " << name.lname << endl << "Phone: " << phone << endl << "Email: " << email << endl << "Designation: " << designation[des-1] << endl << "DOB: " << dob.day << "/" << dob.month << "/" << dob.year << endl;
+            cout << "Faculty Onboarding Successful!" << endl 
+            << "User ID: " << id << endl 
+            << "Name: " << name.fname << " " << name.lname << endl 
+            << "Phone: " << phone << endl 
+            << "Email: " << email << endl 
+            << "Designation: " << designation[desig-1] << endl 
+            << "DOB: " << dob.day << "/" << dob.month << "/" << dob.year << endl 
+            << "Department: " << department << endl;
             cout << "~~~~~~~~~~~~~~~~~~~~~~~~~~~~" << endl;
-
         };
-
-
-
+        friend void insert(int id, faculty *data);
+        friend faculty* search(int id);
 };
+struct node{
+    faculty *data;
+    int id;
+    node *next;
+};
+node* head  = NULL;
 
-int main(){
-    cout << "Welcome to the Examination Grievance System" << endl;
-    int choice;
-    do{
-        cout << "1. Faculty Onboarding" << endl << "2. Faculty Dashboard "<< endl << "3. Student Dashboard "<< endl << "4. Exit" << endl << "Enter your choice: ";
-        cin >> choice;
-        switch(choice){
-            case 1:
-                string fname, lname;
-                cout << "Enter First Name: ";
-                cin >> fname;
-                cout << "Enter Last Name: ";
-                cin >> lname;
-                faculty new_faculty(fname, lname);
-                break;
-            case 2:
-                cout << "Faculty Dashboard" << endl;
-                break;
-            case 3:
-                cout << "Student Dashboard" << endl;
-                break;
-            case 4:
-                cout << "Exiting the system. Goodbye!" << endl;
-                break;
-            default:
-                cout << "Invalid choice. Please try again." << endl;
-        }while(choice != 4);
-
+// Faculty Linked list to store faculty object pointers and their corresponding IDs
+void insert(int id, faculty *data){
+    node* newNode = new node;
+    newNode->id = id;
+    newNode->data = data;
+    newNode->next = NULL;
+    if(head == NULL){
+        head = newNode;
     }
+    else{
+        node* temp = head;
+        while(temp->next != NULL){
+            temp = temp->next;
+        }
+        temp->next = newNode;
+    }
+}
+
+faculty* search(int id){
+    node* temp = head;
+    while(temp != NULL){
+        if(temp->id == id){
+            cout << "Faculty Found: " << temp->data->name.fname << " " << temp->data->name.lname << endl;
+            return temp->data;
+        }
+        temp = temp->next;
+    }
+    cout << "Faculty with ID " << id << " not found." << endl;
+    return NULL;
+}
+int main(){
+    string fname, lname;
+    cout << "Enter First Name: ";
+    cin >> fname;
+    cout << "Enter Last Name: ";
+    cin >> lname;
+    faculty *newFaculty = new faculty(fname, lname);
+    return 0;
 }
