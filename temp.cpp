b8e6205bd0d54014c999cc578aab404f1033fad9
CodeMriDub7 <mritunjaydubey00@gmail.com>
2026-06-03 18:02:57 +0530

    made a user function module

--- temp.cpp
+++ temp.cpp
@@ -1,98 +1,50 @@
 #include <iostream>
-#include <fstream>
 #include <string>
-#include <vector>
 using namespace std;
 
-struct node{
-    int data;
-    node* next;
-};
-node* HEAD = NULL;
-void insertAtHead(int val){
-    node * newNode = new node;
-    newNode->data = val;
-    newNode->next = HEAD;
-    HEAD = newNode;
-}
-void createList(){
-    for(int i = 0; i < 100; i++){
-        insertAtHead(i);
-    }
-}
-void deleteList(){
-    node * temp = HEAD;
-    while(temp != NULL){
-        node * toDelete = temp;
-        temp = temp->next;
-        delete toDelete;
-    }
-    HEAD = NULL;
-}
-void display(){
-    node * temp = HEAD;
-    while(temp != NULL){
-        cout << temp->data << ", "; 
-        temp = temp->next;
-    }
-    cout << "\n endl";
-}
-void changeFile(){
-    fstream facultyData("4137956.csv", ios::trunc | ios::out );
-    node * temp = HEAD;
-    while(temp->next != NULL){
-        facultyData << temp->data << "," << "An element" << endl;
-        temp = temp->next;
-    }
-}
+class message
+{
+public:
+    string messageText[100];
 
-void readFile(){
-    fstream fin;
-
-    // Open an existing file
-    fin.open("4137956.csv", ios::in);
-
-    // Get the roll number
-    // of which the data is required
-
-    // Read the Data from the file
-    // as String Vector
-    vector<string> row;
-    string line, word, temp;
-
-    while (fin >> temp)
+    int storeMessage()
     {
-
-        row.clear();
-
-        // read an entire row and
-        // store it in a string variable 'line'
-        getline(fin, line);
-
-        // used for breaking words
-        stringstream s(line);
-
-        // read every column data of a row and
-        // store it in a string variable, 'word'
-        while (getline(s, word, ','))
+        int choice = 0;
+        int thisline = 0;
+        cout << "press B to go back" << endl;
+        while (choice != 1)
         {
-            // add all the column data
-            // of a row to a vector
-            row.push_back(word);
+            if (thisline == 99)
+            {
+                cout << "Maximum length reached";
+                return 0;
+            }
+            cout << "Enter Message text: ";
+            // cin >> messageText[thisline];
+            getline(cin, messageText[thisline]);
+            if (messageText[thisline] == "b" || messageText[thisline] == "B")
+            {
+                choice = 1;
+            }
+            thisline++;
         }
+        displayMessage();
+        return 0;
     }
-    if (count == 0)
-        cout << "Record not found\n";
-    fin.close();
-}
 
-int main(){
-    createList();
-    changeFile();
-    display();
-    deleteList();
-    display();
-    cout << "~~~~~~~~~~~~~~~~~~~~~~~~~~~lOCAL STORAGE~~~~~~~~~~~~~~~~~~~~~~~~~~" << endl;
-    readFile();
+    void displayMessage()
+    {
+        cout << "~~~~~~~~~~" << endl;
+        for (int i = 0; i < 100; i++)
+        {
+            cout << messageText[i];
+        }
+        cout << "~~~~~~~~~~" << endl;
+    }
+};
+
+int main()
+{
+
     return 0;
 }
