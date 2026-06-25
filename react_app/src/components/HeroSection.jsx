d281068005cf94f1682d222d1ebb6d934a016582
CodeMriDub7 <mritunjaydubey00@gmail.com>
2026-06-25 10:59:57 +0530

    idk some changes

---
+++ react_app/src/components/HeroSection.jsx
@@ -0,0 +1,12 @@
+function HeroSection() {
+  return (
+    <>
+      <section className="heroSection">
+        <h2>Examination Grievance System</h2>
+        <p>Solution for all your examination related problems</p>
+      </section>
+    </>
+  );
+}
+
+export default HeroSection;
