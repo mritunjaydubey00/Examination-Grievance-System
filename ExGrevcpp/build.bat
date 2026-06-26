a78ef73859678f7097736994749139f814264b5c
CodeMriDub7 <mritunjaydubey00@gmail.com>
2026-06-26 18:30:41 +0530

    making the Ui of web application

--- ExGrevcpp/build.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-cd /d "%~dp0"
-g++ *.cpp -o authentication.exe
-if %errorlevel% neq 0 (
-    echo Build failed.
-    pause
-    exit /b %errorlevel%
-)
-echo Build succeeded: authentication.exe
--- ExGrevcpp/localStorage.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-#include <string>
-using namespace std;
-int writeFile(string dataRow, string filename);
-int readFile(string filename);
-int createFile(string fileName);
