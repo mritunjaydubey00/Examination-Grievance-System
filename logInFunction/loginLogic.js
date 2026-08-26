f4686f79e2e7ee7f88deede382e39141ed699235
CodeMriDub7 <mritunjaydubey00@gmail.com>
2026-08-26 11:45:22 +0530

    commit these changes
    some basic changes

---
+++ logInFunction/loginLogic.js
@@ -0,0 +1,9 @@
+function handleLogin(id, password) {
+  const userId = document.getElementById("userId").value;
+  const userPassword = document.getElementById("userPassword").value;
+
+  console.log("User ID:", userId);
+  console.log("User Password:", userPassword);
+}
+
+// Change the id's of Input
